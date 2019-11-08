684dd08b727515d63e6fe1e8f857a00ce56993f8
Cindy Burton <cindy.brton@gmail.com>
2019-11-08 09:19:29 -0700

    I'm having way too much fun

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+// Let's make a quiz! About JavaScript!
+
+// First we need a timer
+    // that timer needs to count down with the ability to remove time based on user input
+
+// Then we need to keep score
+    // score is modified by user input based on correct or incorrect answers
+        // 10 questions worth max 10 points each to = 100
+
+// We need an event to start the game!
+
+// We need a series of questions
+
+ // questions
+
+    // followed by events to track answers given via radio dial button thingie?
+
+    // Then conditions to give points for correct answers
+
+    // Or penalties for incorrect answers
+
+// Once complete we need something spit out score!
+
+// And finally something to store the user's score locally!
+
+var str = "What the fucking fuckity fuck?"
+
+var arr = [str]
+
+console.log(arr)
